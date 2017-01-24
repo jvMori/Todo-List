8349b88735f02cdb9211b98768ab4ec18ab72f4f
Mori <iwona.moryc92@gmail.com>
2017-01-24 19:39:20 +0100

    added toggle func

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,5 @@
 $("li").click(function(){
 
-	if( $(this).css("color") === "rgb(128, 128, 128)") {
-		$(this).css({
-			color: "black", 
-			textDecoration: "none"
-		});
-	} else {
-		$(this).css({
-			color: "gray", 
-			textDecoration: "line-through"
-		});
-	}
+	$(this).toggleClass("done");
+	
 });
