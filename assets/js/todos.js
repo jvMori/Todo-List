a8b4ad0d4024d630824bb52f7274ce398d9a8bf7
Mori <iwona.moryc92@gmail.com>
2017-01-24 20:14:09 +0100

    building it

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,22 @@
-$("li").click(function(){
+$("li").on("click", function(){
 
 	$(this).toggleClass("done");
+
+});
+
+$("span").on("click", function(event){
+	event.stopPropagation();
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+});
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li>" + "<span>X</span> " + todoText + "</li>");
+	}
 	
 });
